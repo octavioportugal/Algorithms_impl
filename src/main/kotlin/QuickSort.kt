cd7c03cffb6e4c303f45071a89493aa97b2e7df4
Octavio Portugal <octavio.portugal@sumup.com>
2024-11-06 14:56:13 -0300

    QuickSort

--- src/main/kotlin/QuickSort.kt
+++ src/main/kotlin/QuickSort.kt
@@ -1,5 +1,5 @@
 fun main(){
-    val arr = intArrayOf(9, 3, 7, 4, 69, 420, 42)
+    val arr = intArrayOf(0)
 
     val result = qs(arr, 0, arr.size - 1)
 
@@ -7,7 +7,7 @@ fun main(){
 }
 
 fun qs(arr: IntArray, lo: Int, hi: Int): IntArray{
-    if (lo >= hi) return intArrayOf()
+    if (lo >= hi) return arr
 
     val pivotIdx = partition(arr, lo, hi)
 
@@ -36,4 +36,42 @@ fun partition(arr: IntArray, lo: Int, hi: Int): Int{
     arr[idx] = pivot
 
     return idx
+}
+
+class LeetCode912 {
+    fun sortArray(nums: IntArray): IntArray {
+        qs(0, nums.size - 1, nums)
+        return nums
+    }
+
+    fun qs(lo: Int, hi: Int, arr: IntArray) {
+        if (lo >= hi) return
+
+        val pivotIdx = partition(lo, hi, arr)
+
+        qs(lo, pivotIdx - 1, arr)
+        qs(pivotIdx, hi, arr)
+    }
+
+    fun partition(lo: Int, hi: Int, arr: IntArray):Int {
+
+        val pivot = arr[(lo + hi) / 2]
+
+        var left = lo
+        var right = hi
+
+        while (left <= right){
+            while (arr[left] < pivot) left++
+            while (arr[right] > pivot) right--
+            if (left <= right){
+                val tmp = arr[left]
+                arr[left] = arr[right]
+                arr[right] = tmp
+                left++
+                right--
+            }
+        }
+
+        return left
+    }
 }
